8c38db8edbb8c3b288ce30b7cea26062a1c127ce
3voke-dev <3voke.1337@gmail.com>
2025-04-03 18:09:24 +0500

    stable version v0.6 fix cards

--- authapp/models.py
+++ authapp/models.py
@@ -21,6 +21,6 @@ class CustomUser(AbstractUser):
         return self.username
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = "User"
+        verbose_name_plural = "Users"
         ordering = ["id"]
